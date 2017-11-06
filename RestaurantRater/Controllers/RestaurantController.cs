88274e9a28d3d485bd7c118f0aa86774a25a43a4
Ciprian Buciumean <cnbucium@gmail.com>
2017-11-06 16:51:57 -0500

    Module 6 Edit and Update

--- RestaurantRater/Controllers/RestaurantController.cs
+++ RestaurantRater/Controllers/RestaurantController.cs
@@ -1,6 +1,7 @@
 ﻿using RestaurantRater.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Net;
 using System.Web;
@@ -28,7 +29,7 @@ namespace RestaurantRater.Controllers
         //POST: Restaurant/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "RestaurantID,Name,Address,Rating")]Restaurant restaurant)
+        public ActionResult Create([Bind(Include = "RestaurantID,Name,Rating,Address")]Restaurant restaurant)
         {
             if (ModelState.IsValid)
             {
@@ -74,6 +75,35 @@ namespace RestaurantRater.Controllers
                 db.Dispose();
             }
             base.Dispose(disposing);
+        }
+
+        //GET: Restaurant/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Restaurant restaurant = db.Restaurants.Find(id);
+            if (restaurant == null)
+            {
+                return HttpNotFound();
+            }
+            return View(restaurant);
+        }
+
+        //POST: Restaurant/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit ([Bind(Include = "Restaurant,Name,Address,Rating")]Restaurant restaurant)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(restaurant).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(restaurant);
         }
 
     }
